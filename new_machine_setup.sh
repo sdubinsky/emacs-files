2a040f9d27a7683fe9905c8e7abecac5d84647d6
Shalom Dubinsky <smdubinsky@gmail.com>
2018-11-19 10:53:23 +0200

    add new machine setup script

---
+++ new_machine_setup.sh
@@ -0,0 +1,37 @@
+sudo apt install curl screen ghc emacs vlc git i3 cowsay fortune-mod postgresql postgresql-server-dev-all cowsay fortune-mod adb feh
+
+git config --global user.name "Shalom Dubinsky"
+git config --global user.email "smdubinsky@gmail.com"
+
+git clone https://github.com/sdubinsky/emacs-files.git
+cp emacs-files/.bash* emacs-files/.profile ~
+cp emacs-files/init.el ~/.emacs.d
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
+ls
+git clone "https://github.com/xflux-gui/fluxgui.git"
+cd fluxgui/
+#python2 download-xflux.py 
+#sudo python2 setup.py install
+cd ~
+
+wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
+cd chruby-0.3.9/
+sudo make install
+cd ~
+
+wget -O ruby-install-0.7.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.7.0.tar.gz
+tar -xzvf ruby-install-0.7.0.tar.gz 
+cd ruby-install-0.7.0/
+sudo make install
+cd ~
+
+ruby-install ruby
+chruby ruby
+gem install bundler
+
+cd ~
+mkdir code
