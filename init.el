52e68370dd73818abcd40e57304e04e0e316b86c
Scott Dubinsky <smdubinsky@gmail.com>
2019-03-18 09:11:21 +0200

    add cycle-spacing

--- init.el
+++ init.el
@@ -45,7 +45,7 @@
  '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
-    (company-nand2tetris nand2tetris nand2tetris-assembler ruby-additional mpdel company-ghc ghc intero omnisharp csharp-mode arduino-mode realgud-byebug realgud-pry go-mode zerodark-theme hc-zenburn-theme yas-global-mode yas-mode diminish feature-mode auto-virtualenv anaconda-mode haskell-mode markdown-mode lua-mode company flycheck ini-mode bundler rspec rvm robe rinari flx-ido web-mode projectile-rails anzu ess lua tuareg use-package haml-mode pianobar names csv-mode yasnippet yaml-mode ruby-tools ruby-end rspec-mode realgud json-mode hi2 guru-mode ghci-completion flymake flycheck-hdevtools f ensime company-inf-ruby browse-kill-ring+ autopair aggressive-indent ac-inf-ruby ac-haskell-process))))
+    (ruby-additional mpdel company-ghc ghc intero omnisharp csharp-mode arduino-mode realgud-byebug realgud-pry go-mode zerodark-theme hc-zenburn-theme yas-global-mode yas-mode yasnippet-snippets diminish feature-mode auto-virtualenv anaconda-mode haskell-mode markdown-mode lua-mode company flycheck ini-mode bundler rspec rvm robe rinari flx-ido web-mode projectile-rails anzu ess lua tuareg use-package haml-mode pianobar names csv-mode yasnippet yaml-mode ruby-tools ruby-end rspec-mode realgud magit json-mode hi2 guru-mode ghci-completion flymake flycheck-hdevtools f ensime company-inf-ruby browse-kill-ring+ autopair aggressive-indent ac-inf-ruby ac-haskell-process))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -82,6 +82,9 @@
 (global-set-key "\C-l" 'goto-line)
 ;;go one fram backward
 (global-set-key (kbd "C-x x") '(lambda () "frame-back one" (interactive) (other-window -1)))
+;; cycle through amounts of spacing - from http://pragmaticemacs.com/emacs/cycle-spacing/
+(global-set-key (kbd "M-SPC") 'cycle-spacing)
+
 (setq nxml-sexp-element-flag t)
 (use-package diminish)
 
