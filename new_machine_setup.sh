0192e85574fe471db07b2c2d8ba4ab9cd8d5a733
Shalom Dubinsky <smdubinsky@gmail.com>
2019-11-19 08:54:11 +0200

    install universal-ctags

--- new_machine_setup.sh
+++ new_machine_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    sudo apt install curl apcalc screen ghc vlc git i3-wm i3status cowsay fortune-mod postgresql postgresql-server-dev-all cowsay adb feh xserver-xorg-input-synaptics redshift exiftool net-tools python python2 syncthing dmenu ripgrep
+    sudo apt install curl apcalc screen ghc vlc git i3-wm i3status cowsay fortune-mod postgresql postgresql-server-dev-all cowsay adb feh xserver-xorg-input-synaptics redshift exiftool net-tools python python2 syncthing dmenu ripgrep universal-ctags
     sudo add-apt-repository ppa:kelleyk/emacs
     sudo apt update && sudo apt -y install emacs26
     sudo rm /usr/bin/emacs
